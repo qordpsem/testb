16e2bc522fc14ff93957cb84426b416bf3698c56
qordpsem <qordpsem@gmail.com>
2024-07-17 16:51:58 +0900

    프로젝트 생성, 상품관리 완료

---
+++ src/GoodsDAO.java
@@ -0,0 +1,9 @@
+class GoodsDAO {
+    public void insert(){
+        System.out.println("insert goods OK");
+    }
+    public void update(){
+        System.out.println("update goods OK");
+    }
+
+}
---
+++ src/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main (String [] args){
+        GoodsDAO dao = new GoodsDAO();
+        dao.insert();
+        dao.update();
+    }
+}
